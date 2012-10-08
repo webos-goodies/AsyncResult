c8ffec6733ac5acac33bf10796750cb3dddc8956
Chihiro Ito <hokousya@sourcewalker.com>
2012-10-08 14:12:31 +0900

    Added an application test target.

---
+++ AsyncResultApplicationTest/AsyncResultApplicationTest.h
@@ -0,0 +1,13 @@
+//
+//  AsyncResultApplicationTest.h
+//  AsyncResultApplicationTest
+//
+//  Created by Chihiro Ito on 2012/10/08.
+//  Copyright (c) 2012年 Chihiro Ito. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AsyncResultApplicationTest : SenTestCase
+
+@end
