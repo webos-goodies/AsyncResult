7c4018011dcc8819e67d6350bf7f55f3b44fe949
Chihiro Ito <hokousya@sourcewalker.com>
2012-10-08 14:05:02 +0900

    Added a logic test target.

---
+++ AsyncResultLogicTest/AsyncResultLogicTest.h
@@ -0,0 +1,13 @@
+//
+//  AsyncResultLogicTest.h
+//  AsyncResultLogicTest
+//
+//  Created by Chihiro Ito on 2012/10/08.
+//  Copyright (c) 2012年 Chihiro Ito. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AsyncResultLogicTest : SenTestCase
+
+@end
---
+++ AsyncResultLogicTest/AsyncResultLogicTest.m
@@ -0,0 +1,32 @@
+//
+//  AsyncResultLogicTest.m
+//  AsyncResultLogicTest
+//
+//  Created by Chihiro Ito on 2012/10/08.
+//  Copyright (c) 2012年 Chihiro Ito. All rights reserved.
+//
+
+#import "AsyncResultLogicTest.h"
+
+@implementation AsyncResultLogicTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AsyncResultLogicTest");
+}
+
+@end
